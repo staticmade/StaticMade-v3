9776c289c32a599921620c3561c0f0b21c5e1d0d
Jeffrey Inscho <jeffrey@staticmade.com>
2014-06-09 22:09:59 -0400

    removed letterboxing on video embeds

--- functions.php
+++ functions.php
@@ -3,3 +3,8 @@
 		'aside', 'image', 'video', 'audio', 'quote', 'link', 'gallery',
 	) );
 }
+
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
--- header.php
+++ header.php
@@ -6,22 +6,18 @@
 <meta name="description" content="<?php bloginfo('description'); ?>">
 <meta name="author" content="Static Made">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
-<meta name="bitly-verification" content="3efbebebbc9f"/>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 <link rel="alternate" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Merriweather:400,700">
 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:300">
 <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
 <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<script async src='https://adnwidgets.herokuapp.com/alpha.js'></script>
 <script src="http://platform.tumblr.com/v1/share.js"></script>
-<script src='https://d2zh9g63fcvyrq.cloudfront.net/adn.js'></script>
-<script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='//d2zh9g63fcvyrq.cloudfront.net/adn.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'adn-button-js'));</script>
 </head>
 <body>
 	<header class="sixteen columns">
 		<div class="header-inner">
-						<div class="title">
+			<div class="title">
 				<h1><a href="<?php echo get_settings('home'); ?>"><?php bloginfo('name'); ?></a></h1>
 				<p>Written + produced with love in the City of Champions.</p>
 				<p>&nbsp;</p>
@@ -29,6 +25,6 @@
 				    <li><a href="/archive">Archive</a></li>
 				    <li><a href="/subscribe">Feeds</a></li>
 				</ul>
-				</div>
+			</div>
 		</div>
 	</header>
